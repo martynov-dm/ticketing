aabf079a45c192c5eac4c4065b7fc72408f101b9
Dmitrii Martynov <dmartynov@griddynamics>
2022-06-21 15:18:58 +0200

    added error handling

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,10 @@
 import express, { Request, Response } from 'express'
 import { body, validationResult } from 'express-validator'
+import {RequestValidationError} from "../errors/request-validation-error";
 
 const router = express.Router()
 
-router.get('/api/users/signup', [
+router.post('/api/users/signup', [
     body('email')
         .isEmail()
         .withMessage('Email must be valid'),
@@ -15,10 +16,10 @@ router.get('/api/users/signup', [
     const errors = validationResult(req)
 
     if(!errors.isEmpty()) {
-        return res.status(400).send(errors.array())
+        throw new RequestValidationError(errors.array())
     }
 
-    const { email, password } = req. body
+    const { email, password } = req.body
 
     console.log('Creating a user')
 
