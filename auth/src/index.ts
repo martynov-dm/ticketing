9714b67c65081ecbf0848dd2b17c32332f3aba29
Dmitrii Martynov <dmartynov@griddynamics>
2022-06-23 10:29:49 +0200

    added password hashing

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,15 @@
 import express from 'express'
+import 'express-async-errors'
 import { json } from 'body-parser'
+import mongoose from "mongoose";
+
 import { currentUserRouter } from "./routes/current-user";
 import {signinRouter} from "./routes/signin";
 import {signoutRouter} from "./routes/signout";
 import {signupRouter} from "./routes/signup";
 import {errorHandler} from "./middlewares/error-handler";
+import {NotFoundError} from "./errors/not-found-error";
+import {DatabaseConnectionError} from "./errors/database-connection-error";
 
 const app = express()
 app.use(json())
@@ -14,8 +19,25 @@ app.use(signinRouter)
 app.use(signoutRouter)
 app.use(signupRouter)
 
+
+app.all('*', () => {
+    throw new NotFoundError()
+})
+
 app.use(errorHandler)
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000!')
-})
+const start = async () => {
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth')
+        console.log('Connected to MongoDb')
+    } catch (e) {
+        console.log(e)
+        throw new DatabaseConnectionError()
+    }
+
+    app.listen(3000, () => {
+        console.log('Listening on port 3000!')
+    })
+}
+
+start()
---
+++ auth/src/models/user.ts
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+import {Password} from "../services/password";
+
+interface UserAttributes {
+   email: string;
+   password: string
+}
+
+interface UserModel extends mongoose.Model<UserDocument> {
+    build(attrs: UserAttributes): UserDocument
+}
+
+interface UserDocument extends mongoose.Document {
+    email: string;
+    password: string
+}
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+userSchema.pre('save', async function (done) {
+    if (this.isModified('password')) {
+        const hashed = await Password.toHash(this.get('password'))
+        this.set('password', hashed)
+    }
+    done()
+})
+
+userSchema.statics.build = (attributes: UserAttributes) => {
+    return new User(attributes)
+}
+
+const User = mongoose.model<UserDocument, UserModel>('User', userSchema)
+
+
+
+export { User }
