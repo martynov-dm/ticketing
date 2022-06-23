10db533607d5ca580ee77b4d96788dcc5d9e852e
Dmitrii Martynov <dmartynov@griddynamics>
2022-06-23 14:38:36 +0200

    added jwt

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -10,9 +10,17 @@ import {signupRouter} from "./routes/signup";
 import {errorHandler} from "./middlewares/error-handler";
 import {NotFoundError} from "./errors/not-found-error";
 import {DatabaseConnectionError} from "./errors/database-connection-error";
+import cookieSession from "cookie-session";
 
 const app = express()
+app.set('trust proxy', true)
 app.use(json())
+app.use(
+    cookieSession({
+        signed: false,
+        secure: true
+    })
+)
 
 app.use(currentUserRouter)
 app.use(signinRouter)
